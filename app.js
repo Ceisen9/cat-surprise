1ea8413e7c3e5157607ac27992b87eed1a488f06
Craig Eisen <Ceisen91@gmail.com>
2016-02-17 13:37:31 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var cat = {
   name: "Frisky 'Cornelius' McWhiskertons",
-  pastimes: "chasing red dots, juding silly humans, and sitting on keyboards",
+  pastimes: "chasing red dots, judging silly humans, and sitting on keyboards",
   start: function() {
     var catImage = document.getElementById("laser_cat");
     catImage.addEventListener("click", function() {
-      console.log("My name is " + this.name + " and I like " + this.pastimes);
-    });
+      alert("My name is " + this.name + " and I like " + this.pastimes);
+    }.bind(this));
   }
 }
 
